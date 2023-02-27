27ae427fd9af5560e556b12b61f7b6c255f485f3
Matheus Henrique de Abreu <matheuslink18@hotmail.com>
2023-02-27 10:59:36 -0300

    Update 3.js
    o JSON atualizado enviado para o meu e-mail

--- 3.js
+++ 3.js
@@ -1,25 +1,125 @@
 const faturamentoMensal = [
-    { "dia": 1, "faturamento": 500 },
-    { "dia": 2, "faturamento": 650 },
-    { "dia": 3, "faturamento": 750 },
-    { "dia": 4, "faturamento": 800 },
-    { "dia": 5, "faturamento": 700 },
-    { "dia": 6, "faturamento": 600 },
-    { "dia": 7, "faturamento": 0 },
-    { "dia": 8, "faturamento": 450 },
-    { "dia": 9, "faturamento": 500 },
-    { "dia": 10, "faturamento": 700 },
-    { "dia": 11, "faturamento": 850 },
-    { "dia": 12, "faturamento": 900 },
-    { "dia": 13, "faturamento": 950 },
-    { "dia": 14, "faturamento": 0 },
-    { "dia": 15, "faturamento": 750 },
-    { "dia": 16, "faturamento": 800 },
-    { "dia": 17, "faturamento": 600 },
-    { "dia": 18, "faturamento": 500 },
-    { "dia": 19, "faturamento": 400 },
-    { "dia": 20, "faturamento": 450 }
-  ];
+	{
+		"dia": 1,
+		"valor": 22174.1664
+	},
+	{
+		"dia": 2,
+		"valor": 24537.6698
+	},
+	{
+		"dia": 3,
+		"valor": 26139.6134
+	},
+	{
+		"dia": 4,
+		"valor": 0.0
+	},
+	{
+		"dia": 5,
+		"valor": 0.0
+	},
+	{
+		"dia": 6,
+		"valor": 26742.6612
+	},
+	{
+		"dia": 7,
+		"valor": 0.0
+	},
+	{
+		"dia": 8,
+		"valor": 42889.2258
+	},
+	{
+		"dia": 9,
+		"valor": 46251.174
+	},
+	{
+		"dia": 10,
+		"valor": 11191.4722
+	},
+	{
+		"dia": 11,
+		"valor": 0.0
+	},
+	{
+		"dia": 12,
+		"valor": 0.0
+	},
+	{
+		"dia": 13,
+		"valor": 3847.4823
+	},
+	{
+		"dia": 14,
+		"valor": 373.7838
+	},
+	{
+		"dia": 15,
+		"valor": 2659.7563
+	},
+	{
+		"dia": 16,
+		"valor": 48924.2448
+	},
+	{
+		"dia": 17,
+		"valor": 18419.2614
+	},
+	{
+		"dia": 18,
+		"valor": 0.0
+	},
+	{
+		"dia": 19,
+		"valor": 0.0
+	},
+	{
+		"dia": 20,
+		"valor": 35240.1826
+	},
+	{
+		"dia": 21,
+		"valor": 43829.1667
+	},
+	{
+		"dia": 22,
+		"valor": 18235.6852
+	},
+	{
+		"dia": 23,
+		"valor": 4355.0662
+	},
+	{
+		"dia": 24,
+		"valor": 13327.1025
+	},
+	{
+		"dia": 25,
+		"valor": 0.0
+	},
+	{
+		"dia": 26,
+		"valor": 0.0
+	},
+	{
+		"dia": 27,
+		"valor": 25681.8318
+	},
+	{
+		"dia": 28,
+		"valor": 1718.1221
+	},
+	{
+		"dia": 29,
+		"valor": 13220.495
+	},
+	{
+		"dia": 30,
+		"valor": 8414.61
+	}
+];
   
   // Ignorando os dias em que o faturamento é 0 (sábado e domingo)
   const diasUteis = faturamentoMensal.filter(dia => dia.faturamento > 0);
